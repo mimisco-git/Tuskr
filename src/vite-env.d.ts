45506a0befa311cfc75911e0370b7306e2c89143
mimisco <mimisco4life@gmail.com>
2026-06-02 04:37:42 +0100

    fix: tsconfig types, vite-env declares all VITE_ vars

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,11 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_PACKAGE_ID: string
-  readonly VITE_MARKETPLACE_ID: string
-  readonly VITE_NETWORK: string
+  readonly VITE_PACKAGE_ID:      string
+  readonly VITE_MARKETPLACE_ID:  string
+  readonly VITE_NETWORK:         string
+  readonly VITE_GROQ_API_KEY:    string
+  readonly VITE_GOOGLE_CLIENT_ID:string
 }
 
 interface ImportMeta {
